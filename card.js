e71bef97bd06d2c658680d2890c9c031ce1ad2b4
pk316 <pr.khiev@gmail.com>
2018-01-15 19:52:48 -0800

    update added more front cards, added function to randomly select 9 images from original array

--- card.js
+++ card.js
@@ -1,21 +1,21 @@
-function Card(frontImage, parentObj){
+function Card(frontImage, parentObj) {
     this.frontImage = frontImage;
     this.parent = parentObj;
     this.revealed = false;
     this.renderedElement = null;
 
-    this.render = function(){
-        var card = $("<div>",{
+    this.render = function () {
+        var card = $("<div>", {
             class: 'card'
         });
         card.click(this.handleClick.bind(this));
-        var front = $("<div>",{
+        var front = $("<div>", {
             class: 'front',
             css: {
-            backgroundImage: `url(${this.frontImage})`
-        }
-    });      
-        var back = $("<div>",{
+                backgroundImage: `url(${this.frontImage})`
+            }
+        });
+        var back = $("<div>", {
             class: 'back',
         })
         this.renderedBack = back;
@@ -24,18 +24,18 @@ function Card(frontImage, parentObj){
         return card;
     }
 
-    this.handleClick = function(){
+    this.handleClick = function () {
         console.log('card clicked');
         this.parent.handleCardClick(this);
     }
 
-    this.revealCard= function(){
+    this.revealCard = function () {
         this.renderedBack.hide();
     }
-    this.hideCard= function(){
+    this.hideCard = function () {
         this.renderedBack.show();
     }
-    this.getId = function(){
+    this.getId = function () {
         return this.frontImage;
     }
 }
--- game.js
+++ game.js
@@ -1,7 +1,5 @@
-
 function MemoryGame(){
-    this.cardsClicked = [];
-    this.resetTime = 1000;
+    this.cardsClickedArray = [];
     this.imageArray = [
         'images/barb.png',
         'images/bomber.png',
@@ -12,149 +10,81 @@ function MemoryGame(){
         'images/pekka.png',
         'images/prince.png',
         'images/wizard.png',
+        'images/archer.png',
+        'images/dragon.png',
+        'images/electroWiz.png',
+        'images/golem.png',
+        'images/knight.png',
+        'images/lumberjack.png',
+        'images/miniPekka.png',
+        'images/musketeer.png',
+        'images/princess.png',
+        'images/skeleton.png',
+        'images/valkrie.png',
+        'images/witch.png'
     ];
     this.initGame = function () {
-        var images = this.imageArray.concat(this.imageArray);
+        var newImageArray = this.selectRandomImages(this.imageArray);
+        var images = newImageArray.concat(newImageArray);
         this.cards = this.createCards(images)
     };
+    this.selectRandomImages = function(imageArray) {
+        var randomImageArray = [];
+        while ( randomImageArray.length < 9 ) {
+            var randomImage = imageArray.splice(this.randomNum(), 1)
+            randomImageArray.push(randomImage);
+        }
+        return randomImageArray;
+    }
+    this.randomNum = function (){
+        return Math.floor(Math.random() * this.imageArray.length)
+    }
     this.createCards = function(images){
-        var cardArray = [];
+        var cardCreatedArray = [];
         for ( var i = 0; i < images.length; i++){
             var newCard = new Card(images[i], this);
             var cardDom = newCard.render();
             $("#gameArea").append(cardDom);
-            cardArray.push(newCard);
+            cardCreatedArray.push(newCard);
         }
-        return cardArray;
+        return cardCreatedArray;
     }
-    this.handleCardClick = function(cardObjClicked){
-
-        if(this.cardsClicked.length<2){
-            this.cardsClicked.push(cardObjClicked);
-            cardObjClicked.revealCard();
+    this.handleCardClick = function(cardClicked){
+        console.log('card clicked', this)
+        if(this.cardsClickedArray.length<2){
+            if (this.cardsClickedArray[0] === cardClicked) {
+                return;
+            }
+            this.cardsClickedArray.push(cardClicked);
+            cardClicked.revealCard();
         }
-        if (this.cardsClicked.length === 2){
-            if(this.cardsClicked[0].getId() === this.cardsClicked[1].getId()){
+        if (this.cardsClickedArray.length === 2){
+            if(this.cardsClickedArray[0].getId() === this.cardsClickedArray[1].getId()){
                 console.log('match!!!!!');
                 this.matchCount += 2;
                 if(this.matchCount === this.cards.length){
-                    this.playerWins();
+                    this.winner();
                 }
                 this.clearCardsClicked();
             } else {
-                setTimeout(this.resetCardsClicked.bind(this), this.resetTime);
+                setTimeout(this.resetCardsClicked.bind(this), 1000);
             }
         }
 
-        this.playerWins = function(){
+        this.winner = function(){
             alert('you win!');
         }
         
         this.resetCardsClicked = function () {
-            for (var i = 0; i < this.cardsClicked.length; i++) {
-                this.cardsClicked[i].hideCard();
+            for (var i = 0; i < this.cardsClickedArray.length; i++) {
+                this.cardsClickedArray[i].hideCard();
             }
             this.clearCardsClicked();
         };
         this.clearCardsClicked = function () {
-            this.cardsClicked = [];
+            this.cardsClickedArray = [];
         };
 
     }
 }
-
-
-
-
-
-
-//     this.imageArray =[
-//         'images/barb.png', 
-//         'images/archer.png',
-//         'images/hog.png', 
-//         'images/icewiz.png',
-//         'images/giant.png', 
-//         'images/gob.png', 
-//         'images/prince.png', 
-//         'images/bomber.png',
-//         'images/pekka.png',
-//     ];
-
-//     this.shuffleCards = function (imageArray) {
-//         var imageArray = imageArray.concat(imageArray);
-//         var currentIndex = imageArray.length, temp, randomIndex;
-//         while (0 !== currentIndex) {
-//             randomIndex = Math.floor(Math.random() * currentIndex);
-//             currentIndex -= 1;
-//             temp = imageList[currentIndex];
-//             imageArray[currentIndex] = imageArray[randomIndex];
-//             imageArray[randomIndex] = temp;
-//         }
-//         return imageArray;
-//     };
-
-//     this.createBoard = function (imageArray) {
-//         for ( var i = 0; i < imageArray.length; i++){
-//             var card = new Card(this, imageArray[i]);
-//             var cardElement = card.render();
-//             $('.game-container').append(cardElement);
-//             this.images.push(card);
-//         }
-//         return this.cardList;
-//         }
-//     }
-// }
-
-// function Card(parentObj, frontImage) {
-//     this.parent = parentObj;
-//     this.frontImage = frontImage;
-
-//     //Creates the DOM elements for the card deck and is instantiated in the Game object
-//     this.render = function () {
-//         var card = $('<div>', {
-//             class: 'card'
-//         });
-//         var flipContainer = $('<div>', {
-//             class: 'flip-container'
-//         });
-//         var front = $('<div>', {
-//             class: 'front',
-//             css: {
-//                 backgroundImage: 'url(' + this.frontImage + ')',
-//             }
-//         });
-//         var back = $('<div>', {
-//             class: 'back'
-//         });
-//         card.append(flipContainer);
-//         flipContainer.append(front, back);
-//         card.click(this.handleClick.bind(this));
-//         this.flipContainer = flipContainer;
-//         this.card = card;
-//         return card;
-//     };
-
-//     //Handles the cards clicked and updates the stats
-//     this.handleClick = function () {
-//         this.parent.handleCardClick(this);
-//         this.parent.updateStats();
-//     };
-
-//     //Hides the back of the card, and adds the flip class for animation
-//     this.revealSelf = function () {
-//         this.flipContainer.toggleClass('flipped');
-//         this.card.toggleClass('revealed');
-//     };
-
-//     //Shows the back of the card and removes the flip animation 
-//     this.hideSelf = function () {
-//         this.flipContainer.removeClass('flipped');
-//         this.card.removeClass('revealed');
-//     };
-
-//     //Targets the front image
-//     this.getID = function () {
-//         return this.frontImage;
-//     };
-// }
 
